1e8a73b3727c2ce88af224fb630fc2cf13aebdd2
zerosanityy <akosikenneth099@gmail.com>
2022-04-01 23:25:07 +0800

    prefab folder+ hazard

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class H : MonoBehaviour
+{
+    private Transform player;
+     
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(PlayerDed());
+        }
+    }
+        
+     
+        IEnumerator PlayerDed()
+        {
+        player.GetComponent<MeshRenderer>().enabled = false;
+        control playerControls = player.GetComponent<control>();
+        playerControls.enabled = false;
+
+        yield return new WaitForSeconds(2f);
+
+        //respawn
+        player.transform.position = new Vector3(52, -96 , -28); 
+        player.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        player.GetComponent<MeshRenderer>().enabled = true;
+
+
+    }
+    }
+
